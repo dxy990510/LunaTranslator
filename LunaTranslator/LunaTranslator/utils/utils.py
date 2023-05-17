118913ed2934ae3cca8b5d88f3f03d17900459fc
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2023-05-17 16:44:05 +0800

    修正
    Update static_data.json
    
    Update static_data.json
    
    修正
    
    Update dialog_savedgame.py

--- LunaTranslator/LunaTranslator/utils/utils.py
+++ LunaTranslator/LunaTranslator/utils/utils.py
@@ -232,25 +232,6 @@ def minmaxmoveobservefunc(self):
                         pass
                   
                 time.sleep(0.1)
-def le3264run(game,alwaysuselr): 
-    try:
-        b=win32utils.GetBinaryType(game)
-        if b==0 and alwaysuselr==False: 
-            #le=os.path.join(os.path.abspath(globalconfig['LocaleEmulator']),'LEProc.exe')
-            le=(os.path.abspath('./files/plugins/shareddllproxy32.exe'))
-            if os.path.exists(le): 
-                    win32utils.CreateProcess(None,f'"{le}" le "{(game)}"', None,None,False,0,None, os.path.dirname(game), win32utils.STARTUPINFO()  ) 
-        elif b==6 or alwaysuselr==True: 
-                #le=os.path.join(os.path.abspath(globalconfig['Locale_Remulator']),'LRProc.exe')
-                le=os.path.join(os.path.abspath('./files/plugins/Locale_Remulator'),'LRProc.exe')
-                if os.path.exists(le):
-                      win32utils.CreateProcess(None,f'"{le}" 5f4c9504-8e76-46e3-921b-684d7826db71 "{(game)}"', None,None,False,0,None, os.path.dirname(game), win32utils.STARTUPINFO()  ) 
-
-    except:
-        win32utils.CreateProcess(game,None, None,None,False,0,None, os.path.dirname(game), win32utils.STARTUPINFO()  ) 
-    
-
-
 
 def update():
     with open('./cache/update/update.bat','w',encoding='utf8') as ff:
